10d818a6e213f187c179e88797a1a2378d898ed7
Your Name <you@example.com>
2026-02-25 13:19:30 -0500

    Updated Redux store and package configuration

--- src/store.js
+++ src/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+import venueReducer from './venueSlice';
+import avReducer from './avSlice';
+import mealsReducer from './mealsSlice';
+
+export default configureStore({
+  reducer: {
+    venue: venueReducer,
+    av: avReducer,
+    meals: mealsReducer,
+  },
 });
-export default store
+
